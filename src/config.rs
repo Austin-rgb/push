5d245194766bec1c01b52503651aaa2f007abe45
Ochieng Austine <114088922+Austin-rgb@users.noreply.github.com>
2026-04-18 15:18:44 +0300

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -52,7 +52,7 @@ impl Config {
         };
         Self { state }
     }
-    fn config(self, cfg: &mut ServiceConfig) {
+    pub fn config(self, cfg: &mut ServiceConfig) {
         cfg.service(web::scope("").app_data(self.state).service(ws_route));
     }
 }
