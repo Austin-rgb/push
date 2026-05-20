2b2bba204c9bd2c4d04f7f3b696ea14191209118
Austin-rgb <prof.odhiambo.ostine@gmail.com>
2026-05-21 00:15:09 +0300

    integrated event stream

--- src/config.rs
+++ src/config.rs
@@ -1,17 +1,33 @@
 use crate::ws::{ChatServer, MessageOnTrans, Service, deliver_message, ws_route};
-use actix::Actor;
+use actix::{Actor, Addr};
 use actix_web::web::{self, ServiceConfig};
 
 use actixutils::{Identity, Validate};
-use event_stream::{EventStream, OrphanWrapper};
+use event_stream::{EventMetaData, EventStream, Handler, OrphanWrapper};
 use serde::{Deserialize, Serialize};
-use serde_json::from_str;
+use serde_json::{Value, from_str};
 use std::sync::Arc;
 use uuid::Uuid;
 
-#[derive(Serialize)]
-struct Report {
-    delivered: bool,
+struct OnNotification {
+    pub push: Addr<ChatServer>,
+}
+
+#[async_trait::async_trait]
+impl Handler for OnNotification {
+    async fn handle(&self, subject: String, message: Vec<u8>) {
+        let message = String::from_utf8(message).unwrap();
+        let event: Value = from_str(&message).unwrap();
+        let metadata: EventMetaData =
+            from_str(event.get("metadata").unwrap().as_str().unwrap()).unwrap();
+
+        let notification: NotificationRequest = from_str(&message).unwrap();
+        deliver_message(
+            &MessageOnTrans::new(metadata.producer, notification.message),
+            notification.targets,
+            self.push.clone(),
+        );
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -49,9 +65,11 @@ impl Config {
     ) -> Self {
         let chat_server = ChatServer::new().start();
         let state = Service {
-            chat_server,
+            chat_server: chat_server.clone(),
             authv: validator.0,
         };
+        let handler = OnNotification { push: chat_server };
+        es.0.subscribe(">".to_string(), Arc::new(handler));
         Self { state }
     }
     pub fn config(&self, cfg: &mut ServiceConfig, namespace: &str) {
