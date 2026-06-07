ec79026380bf0a507eea309a3d831ab1d2d7624c
Austin-rgb <prof.odhiambo.ostine@gmail.com>
2026-06-07 22:06:27 +0300

    removed event tracking

--- src/config.rs
+++ src/config.rs
@@ -9,27 +9,6 @@ use serde_json::{Value, from_str};
 use std::sync::Arc;
 use uuid::Uuid;
 
-struct OnNotification {
-    pub push: Addr<ChatServer>,
-}
-
-#[async_trait::async_trait]
-impl Handler for OnNotification {
-    async fn handle(&self, _subject: String, message: Vec<u8>) {
-        let message = String::from_utf8(message).unwrap();
-        let event: Value = from_str(&message).unwrap();
-        let metadata: EventMetaData =
-            from_str(event.get("metadata").unwrap().as_str().unwrap()).unwrap();
-
-        let notification: NotificationRequest = from_str(&message).unwrap();
-        deliver_message(
-            &MessageOnTrans::new(metadata.producer, notification.message),
-            notification.targets,
-            self.push.clone(),
-        );
-    }
-}
-
 #[derive(Serialize, Deserialize)]
 pub struct NotificationRequest {
     pub message: String,
@@ -61,7 +40,6 @@ impl Config {
         );
     }
     pub async fn new(
-        es: Arc<dyn EventStream>,
         validator: Arc<dyn Validate<Identity>>,
     ) -> Self {
         let chat_server = ChatServer::new().start();
@@ -69,13 +47,7 @@ impl Config {
             chat_server: chat_server.clone(),
             authv: validator,
         };
-        let handler = OnNotification { push: chat_server };
-        match es.subscribe(">".to_string(), Arc::new(handler)).await {
-            Ok(_) => (),
-            Err(e) => {
-                eprintln!("subscription failed: {e}");
-            }
-        };
+        
         Self { state }
     }
     pub fn config(&self, cfg: &mut ServiceConfig, namespace: &str) {
